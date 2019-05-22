cb1a5d17c8a8dba928fa952eaa889b31d56e0b1f
Joshua KWAYIBA <jkwayiba@c4r21s2.wethinkcode.co.za>
2019-05-22 15:10:00 +0200

    update to memcmp

--- My_libft/functions/ft_memcmp/ft_memcmp.c
+++ My_libft/functions/ft_memcmp/ft_memcmp.c
@@ -1,13 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkwayiba <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/22 14:42:47 by jkwayiba          #+#    #+#             */
+/*   Updated: 2019/05/22 14:43:49 by jkwayiba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <string.h>
 
 int	memcmp(const void *s1, const void *s2, size_t n)
 {
-	const unsigned char *p1 = s1;
-	const unsigned char *p2 = s2;
+	const unsigned char *p1;
+	const unsigned char *p2;
+
+	*p1 = s1;
+	*p2 = s2;
 	while (n--)
 		if (*p1 != *p2)
-			return(*p1 - *p2);
+			return (*p1 - *p2);
 		else
-			p1++,p2++;
-	return(0);
+		{
+			p1++;
+			p2++;
+		}
+	return (0);
 }
