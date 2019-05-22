26c4741e07d460d576ab80471f2574a9a9f92347
Joshua KWAYIBA <jkwayiba@c4r21s2.wethinkcode.co.za>
2019-05-22 14:55:37 +0200

    update to memccpy

--- My_libft/functions/ft_memccpy/ft_memccpy.c
+++ My_libft/functions/ft_memccpy/ft_memccpy.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkwayiba <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/22 14:17:48 by jkwayiba          #+#    #+#             */
+/*   Updated: 2019/05/22 14:19:17 by jkwayiba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <string.h>
 
-void	*ft_memccpy(void *dest, const void *src, int c; size_t n)
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
 	unsigned char *s2;
 	unsigned char *d2;
@@ -10,13 +22,11 @@ void	*ft_memccpy(void *dest, const void *src, int c; size_t n)
 	while (n != 0)
 	{
 		*d2 = *s2;
-		if (*s2 == (unsigned char)c
-				return (d2 +1);
-			d2++;
-			s2++;
-			n--;
+		if (*s2 == (unsigned char)c)
+			return (d2 + 1);
+		d2++;
+		s2++;
+		n--;
 	}
-	reurn (NULL);
+	return (NULL);
 }
-
-
