73d2e6b26e02beea4b976c9a85e24fe19472f63c
Joshua KWAYIBA <jkwayiba@c4r21s2.wethinkcode.co.za>
2019-05-22 15:08:13 +0200

    update to memchr

--- My_libft/functions/ft_memchr/ft_memchr.c
+++ My_libft/functions/ft_memchr/ft_memchr.c
@@ -1,10 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkwayiba <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/22 14:32:53 by jkwayiba          #+#    #+#             */
+/*   Updated: 2019/05/22 14:38:28 by jkwayiba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <string.h>
 
 void	*memchr(const void *s, int c, size_t n)
 {
-	unisgned char *p = (unsigned char*)s;
-	while( n-- )
-		if( *p != (unsigned char)c)
+	unsigned char *p;
+
+	*p = (unsigned char*)s;
+	while (n--)
+		if (*p != (unsigned char)c)
 			p++;
 		else
 			return (p);
