55ac35935dca862893a8396023a5d6159f4f18e5
Joshua KWAYIBA <jkwayiba@c4r21s2.wethinkcode.co.za>
2019-05-23 09:54:23 +0200

    update to strncat

--- My_libft/functions/ft_strncat/ft_strncat.c
+++ My_libft/functions/ft_strncat/ft_strncat.c
@@ -1,8 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkwayiba <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/23 09:50:37 by jkwayiba          #+#    #+#             */
+/*   Updated: 2019/05/23 09:52:35 by jkwayiba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <string.h>
 
 char	*strncat(char *dest, const char *src, size_t n)
 {
-	char *ret = dest;
+	char *ret;
+
+	*ret = dest;
 	while (*dest)
 		dest++;
 	while (n--)
