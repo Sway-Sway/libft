87fd7438e86963b64dac0d6c547c0378427b8727
Joshua KWAYIBA <jkwayiba@c4r21s2.wethinkcode.co.za>
2019-05-23 12:30:10 +0200

    update to strcmp

--- My_libft/functions/ft_strcmp/ft_strcmp.c
+++ My_libft/functions/ft_strcmp/ft_strcmp.c
@@ -1,7 +1,16 @@
-#include <unistd.h>
-#include <string.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkwayiba <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/23 12:26:57 by jkwayiba          #+#    #+#             */
+/*   Updated: 2019/05/23 12:29:37 by jkwayiba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int    ft_strcmp(char *s1, char *s2)
+int		ft_strcmp(char *s1, char *s2)
 {
 	int i;
 	int s1_total;
@@ -10,13 +19,13 @@ int    ft_strcmp(char *s1, char *s2)
 	i = 0;
 	s1_total = 0;
 	s2_total = 0;
-	while ( s1[i] != '\0')
+	while (s1[i] != '\0')
 	{
-		if ( s1[i] != s2[i])
+		if (s1[i] != s2[i])
 		{
 			s1_total = s1[i];
 			s2_total = s2[i];
-			break;
+			break ;
 		}
 		i++;
 	}
