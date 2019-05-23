b7a8873ab6abdd59831d5c7a026f77d4a7a6e9fe
Joshua KWAYIBA <jkwayiba@c4r21s2.wethinkcode.co.za>
2019-05-23 08:54:08 +0200

    update of memcpy

--- My_libft/functions/ft_memcpy/ft_memcpy.c
+++ My_libft/functions/ft_memcpy/ft_memcpy.c
@@ -1,10 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkwayiba <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/23 08:51:50 by jkwayiba          #+#    #+#             */
+/*   Updated: 2019/05/23 08:53:20 by jkwayiba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <string.h>
 
 void	*memcpy(void *dest, const void *src, size_t n)
 {
-	char *dp = dest;
-	const char *sp = src;
-	while ( n--)
+	char		*dp;
+	const char	*sp;
+
+	*dp = dest;
+	*sp = src;
+	while (n--)
 		*dp++ = *sp++;
 	return (dest);
 }
