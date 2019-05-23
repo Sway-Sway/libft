53fa216d1b1219ad560fa96c6c71e1bca67f5021
Joshua KWAYIBA <jkwayiba@c4r21s2.wethinkcode.co.za>
2019-05-23 09:19:00 +0200

    update to memmove

--- My_libft/functions/ft_memmove/ft_memmove.c
+++ My_libft/functions/ft_memmove/ft_memmove.c
@@ -1,7 +1,19 @@
-void *ft_memmove(void *dest, void const *src, size_t n)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkwayiba <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/23 08:57:57 by jkwayiba          #+#    #+#             */
+/*   Updated: 2019/05/23 09:18:07 by jkwayiba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memmove(void *dest, void const *src, size_t n)
 {
-	chat *dest2;
-	
+	char *dest2;
+
 	dest2 = (char*)malloc(sizeof(char) * n + 1);
 	dest2[n + 1] = '\0';
 	ft_memcpy(dest2, src, n);
