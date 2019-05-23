933736a97112a9fbb1b32c6d0020efd5b92f3ab7
Joshua KWAYIBA <jkwayiba@c4r21s2.wethinkcode.co.za>
2019-05-23 08:47:34 +0200

    update to memset

--- My_libft/functions/ft_memset/ft_memset.c
+++ My_libft/functions/ft_memset/ft_memset.c
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkwayiba <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/23 08:40:18 by jkwayiba          #+#    #+#             */
+/*   Updated: 2019/05/23 08:46:39 by jkwayiba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <string.h>
 
 void	ft_memset(void *s, int c, size_t len)
 {
-	unisgned char *ptr = s;
+	unsigned char *ptr;
 
-	while(len--)
+	*ptr = s;
+	while (len--)
 		*ptr++ = (unsigned char)c;
 	return (s);
 }
-
