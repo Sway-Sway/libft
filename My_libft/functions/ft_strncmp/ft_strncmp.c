ac4defb0337d2dfb627a06ce5004ec038eb50674
Joshua KWAYIBA <jkwayiba@c4r21s2.wethinkcode.co.za>
2019-05-24 08:13:18 +0200

    update to strncmp

--- My_libft/functions/ft_strncmp/ft_strncmp.c
+++ My_libft/functions/ft_strncmp/ft_strncmp.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkwayiba <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/23 12:30:55 by jkwayiba          #+#    #+#             */
+/*   Updated: 2019/05/24 08:10:37 by jkwayiba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <string.h>
 
-int	strncmp(const char *s1, comst char *s2, size_t n)
+int	strncmp(const char *s1, const char *s2, size_t n)
 {
-	while(n--)
-		if(*s1++ != *s2++)
-			return *(unsigned char*)(s1 - 1) - *(unsigned char*)(s2 -1);
+	while (n--)
+		if (*s1++ != *s2++)
+			return (*(unsigned char*)(s1 - 1) - *(unsigned char*)(s2 - 1));
 	return (0);
 }
