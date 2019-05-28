7a25159f14b3f1822ca66067cf0138b618d46804
Joshua KWAYIBA <jkwayiba@c4r21s2.wethinkcode.co.za>
2019-05-28 11:12:25 +0200

    update to header file

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: jkwayiba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 09:27:28 by jkwayiba          #+#    #+#             */
-/*   Updated: 2019/05/27 10:44:13 by jkwayiba         ###   ########.fr       */
+/*   Updated: 2019/05/28 11:10:45 by jkwayiba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,11 +39,16 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, void const *src, size_t n);
 void	*ft_memset(void *s, int c, size_t len);
 void	ft_putchar(char c);
+void	ft_putchar_fd(char c, int fd);
 void	ft_putnbr(int nb);
-void	ft_putstr(char *str);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr(char const *str);
+void	ft_putstr_fd(char const *s, int fd);
+void	ft_putendl(char const *s);
+void	ft_putendl_fd(char const *s, int fd);
 char	*ft_strcat(char *dest, char *src);
 char	*ft_strchr(const char *s, int c);
-int		ft_strcmp(char *s1, char *s2);
+int		ft_strcmp(const char *s1,const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strcpy(char *s1, char *s2);
 char	*ft_strdup(char *src);
