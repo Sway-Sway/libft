ae8f477c3b4d6b70785b2be2da355d7b7631f27d
Joshua KWAYIBA <jkwayiba@c4r21s2.wethinkcode.co.za>
2019-06-11 17:25:05 +0200

    update to srcs

--- src/ft_putstr.c
+++ src/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: jkwayiba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 08:27:41 by jkwayiba          #+#    #+#             */
-/*   Updated: 2019/06/11 15:50:43 by jkwayiba         ###   ########.fr       */
+/*   Updated: 2019/06/11 16:17:58 by jkwayiba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,4 @@ void	ft_putstr(char const *str)
 	if (!(str))
 		return ;
 	write(1, str, ft_strlen(str));
-}
-
-int	main(void)
-{
-	ft_putstr("I hope this works");
 }
--- src/ft_strmap.c
+++ src/ft_strmap.c
@@ -1,18 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*   ft_strmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jkwayiba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/24 08:36:53 by jkwayiba          #+#    #+#             */
-/*   Updated: 2019/06/11 17:21:22 by jkwayiba         ###   ########.fr       */
+/*   Created: 2019/06/11 17:04:32 by jkwayiba          #+#    #+#             */
+/*   Updated: 2019/06/11 17:23:33 by jkwayiba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 
-void	ft_putchar(char c)
+char	*ft_strmap(const char *s, char (*f)(char))
 {
-	write(1, &c, 1);
+	int i;
+	char *str;
+
+	if (!s)
+		return (NULL);
+	if (!(str = (char *)malloc(sizeof(char) * ft_strlen(s) + 1)))
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = f(s[i]);
+		i +=1;
+	}
+	str[i] = '\0';
+	return (str);
 }
