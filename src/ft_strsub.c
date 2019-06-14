356964e4fd8f280fe4778ed75629461fcc480117
Joshua KWAYIBA <jkwayiba@c4r21s2.wethinkcode.co.za>
2019-06-14 16:46:41 +0200

    update to srcs

---
+++ src/ft_strsub.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkwayiba <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/14 16:41:28 by jkwayiba          #+#    #+#             */
+/*   Updated: 2019/06/14 16:46:20 by jkwayiba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+char	*ft_strsub(const char *s, unsigned int start, size_t len)
+{
+	char *str;
+
+	if (!s || !(str = ft_strnew(len)))
+		return (NULL);
+	ft_memset(str,'\0', len);
+	ft_strncpy(str, s + start, len);
+	return ((char *)str);
+}
